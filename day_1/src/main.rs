17203cced567f80e42c999f87b2be9c114718467
Nate <78760239+NateNoNameSOFT@users.noreply.github.com>
2022-09-08 14:25:44 -0700

    add part 1 of day one solution

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,3 +1,18 @@
+use std::{fs, cmp::Ordering};
+
 fn main() {
-    println!("Hello, world!");
+    let input: Vec<isize> = fs::read_to_string("input")
+        .expect("Could not read file")
+        .split_whitespace()
+        .map(|num| num.trim().parse::<isize>().unwrap() )
+        .collect();
+
+    let mut larger_than_prev = 0;
+    for (last_depth, current_depth) in input.iter().zip(input.iter().skip(1)) {
+        if current_depth.cmp(&last_depth) == Ordering::Greater {
+            larger_than_prev += 1;
+        }
+    }
+
+    println!("Amount of measurments larger than the previous: {larger_than_prev}");
 }
